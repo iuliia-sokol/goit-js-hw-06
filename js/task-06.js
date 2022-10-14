5952022dec15822429691f68871234517a81775a
Iuliia Sokol <avare779@gmail.com>
2022-10-14 13:53:55 +0300

    Task 6 done

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,16 @@
 // Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+const inputEl = document.getElementById("validation-input");
+
+inputEl.addEventListener("blur", (event) => {
+  if (inputEl.value.length == inputEl.dataset.length) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  }
+  if (inputEl.value.length != inputEl.dataset.length) {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+});
