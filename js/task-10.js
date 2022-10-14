4b8542cc043b2ee7bc1be9a626ca35cc036475a6
Iuliia Sokol <avare779@gmail.com>
2022-10-14 18:53:39 +0300

    Task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,53 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить
+//  кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція.
+//  Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число.
+//  Функція створює стільки < div >, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX.
+// Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+const inputEl = document.querySelector("input");
+const btnCreateEl = document.querySelector("[data-create]");
+const btnDestroyEl = document.querySelector("[data-destroy]");
+
+console.dir(typeof inputEl.value);
+
+const boxesContainerEl = document.getElementById("boxes");
+boxesContainerEl.style.display = "flex";
+boxesContainerEl.style.flexWrap = "wrap";
+boxesContainerEl.style.gap = "10px";
+boxesContainerEl.style.marginTop = "20px";
+
+btnCreateEl.addEventListener("click", () => {
+  createBoxes(+inputEl.value);
+});
+
+btnDestroyEl.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const divArray = [];
+  const basicWidth = 30;
+  const basicHeight = 30;
+  for (let i = 0; i < amount; i++) {
+    const divEL = document.createElement("div");
+    divEL.style.backgroundColor = getRandomHexColor();
+    divEL.style.width = `${basicWidth + 10 * i}px`;
+    divEL.style.height = `${basicHeight + 10 * i}px`;
+    divArray.push(divEL);
+  }
+  return boxesContainerEl.append(...divArray);
+}
+
+function destroyBoxes() {
+  return (boxesContainerEl.innerHTML = "");
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
