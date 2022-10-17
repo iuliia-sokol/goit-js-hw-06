16d74b37c50d8288ce93048539dbf7c4a07e4400
Iuliia Sokol <avare779@gmail.com>
2022-10-17 18:20:31 +0300

    Task 7 fixed

--- js/task-07.js
+++ js/task-07.js
@@ -5,7 +5,6 @@
 const inputEl = document.getElementById("font-size-control");
 const textEl = document.getElementById("text");
 
-inputEl.addEventListener("change", () => {
-  console.log(inputEl.value);
+inputEl.addEventListener("input", () => {
   textEl.style.fontSize = inputEl.value + "px";
 });
